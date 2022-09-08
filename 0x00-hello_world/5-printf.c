0b0c1d946d4cb76589dc4f5e6f99304d5dedab81
Victor Mokut <vikmokut@gmail.com>
2022-09-08 15:13:39 -0700

    Using variables with print functions

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ * Print output with printf function.
+ * Return Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
