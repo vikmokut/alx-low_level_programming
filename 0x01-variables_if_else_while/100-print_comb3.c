1434bacc722715aba45ed3868ffafe7396c1aeb4
Victor Mokut <vikmokut@gmail.com>
2022-09-09 15:56:52 -0700

    combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry Point
+ *
+ * Description: print all possible different
+ * 	combination of two digits.
+ *
+ * Return: Always 0 (Success)
+*/
+
+in main (void)
+{
+	int digit1 = 0, digit2;
+
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digits2 <- 9)
+		{
+			if (digit1 != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
+
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++digit2;
+		}
+		++digit1;
+	}
+	putchar('\n');
+
+	return (0);
+}
