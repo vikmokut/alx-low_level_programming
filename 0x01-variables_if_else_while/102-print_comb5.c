59a77bbc6edc152b30fd7cba4b8c25f7c6debcb5
Victor Mokut <vikmokut@gmail.com>
2022-09-09 16:37:46 -0700

    combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all posible different combinations
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int firstDegit
+
+	return 0;
+}
