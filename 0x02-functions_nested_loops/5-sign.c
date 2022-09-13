c062c1461172d9cafe98198bc43703c09e6fc278
Victor Mokut <vikmokut@gmail.com>
2022-09-13 11:41:30 -0700

    Prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - Entr point
+ * print_sign - Entry point
  * @n: carrier variable
  * Description: prints the sign of a number
  * Return: 1 if n>0, 0 if n == 0, -1 if n<0
@@ -9,16 +9,17 @@
 
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n < 0)
-{
-_putchar('-');
-return (-1);
-}
-_putchar('0');
-return (0);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+
+	return (0);
 }
