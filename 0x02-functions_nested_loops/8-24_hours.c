663754275d8362907bbfe6ce6380732f112d8275
Victor Mokut <vikmokut@gmail.com>
2022-09-13 11:50:52 -0700

    Prints every minutes of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * jack_bauer - Entry point
  * Description: prints every minute of the day
  * Return: void
  */
+
 void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
