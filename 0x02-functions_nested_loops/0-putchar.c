149bdf7b488f23f867e187b0241b63ce0024fa47
Victor Mokut <vikmokut@gmail.com>
2022-09-13 08:52:03 -0700

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar using putchar prototype
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
