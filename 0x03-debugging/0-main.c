ec8e7c8a382221f1a0494aebf62d9cc5ba9c88e7
Victor Mokut <vikmokut@gmail.com>
2022-09-14 21:48:01 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Description: tests function that prints if
- * integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
@@ -12,7 +10,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
